7c5e25e0e59fd88e98bae0147b97f78552ba07a3
Jeffrey Su <zsu@senparc.com>
2018-11-02 16:33:31 +0800

    添加消息服务代码

---
+++ src/NeuCharAppSample/NeuCharAppSample/MessageHandlers/AppMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using Senparc.NeuChar.Entities;
+using Senparc.NeuChar.Helpers;
+using Senparc.Weixin.MP.AppStore;
+using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MessageHandlers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeuCharAppSample.MessageHandlers
+{
+    public class AppMessageHandler : MessageHandler<AppMessageContext>
+    {
+        public AppMessageHandler(Stream inputStream, PostModel postModel, int maxRecordCount = 0, DeveloperInfo developerInfo = null) 
+            : base(inputStream, postModel, maxRecordCount, developerInfo)
+        {
+        }
+
+        public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        {
+            var responseMessage = requestMessage.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "无法识别您的请求，请输入任意类型的文字（或数字）！";
+            return responseMessage;
+        }
+    }
+}
--- src/NeuCharAppSample/NeuCharAppSample/Startup.cs
+++ src/NeuCharAppSample/NeuCharAppSample/Startup.cs
@@ -8,6 +8,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Senparc.CO2NET;
+using Senparc.CO2NET.RegisterServices;
+using Senparc.Weixin.Entities;
+using Senparc.Weixin.RegisterServices;
 
 namespace NeuCharAppSample
 {
@@ -32,10 +37,23 @@ namespace NeuCharAppSample
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddMemoryCache();//使用本地缓存必须添加
+            services.AddSession();//使用Session
+
+            /*
+             * CO2NET 是从 Senparc.Weixin 分离的底层公共基础模块，经过了长达 6 年的迭代优化，稳定可靠。
+             * 关于 CO2NET 在所有项目中的通用设置可参考 CO2NET 的 Sample：
+             * https://github.com/Senparc/Senparc.CO2NET/blob/master/Sample/Senparc.CO2NET.Sample.netcore/Startup.cs
+             */
+
+            services.AddSenparcGlobalServices(Configuration)//Senparc.CO2NET 全局注册
+                    .AddSenparcWeixinServices(Configuration);//Senparc.Weixin 注册
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IOptions<SenparcSetting> senparcSetting, IOptions<SenparcWeixinSetting> senparcWeixinSetting)
         {
             if (env.IsDevelopment())
             {
@@ -55,6 +73,9 @@ namespace NeuCharAppSample
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            // 启动 CO2NET 全局注册，必须！
+            IRegisterService register = RegisterService.Start(env, senparcSetting.Value).UseSenparcGlobal();
         }
     }
 }
