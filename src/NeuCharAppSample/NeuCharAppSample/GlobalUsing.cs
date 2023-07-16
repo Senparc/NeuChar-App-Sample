19c4584e155b97856bffd75011cb1828346e34a6
苏震巍 <zsu@senparc.com>
2023-07-16 15:14:11 +0800

    更新 MVC 框架

--- src/NeuCharAppSample/NeuCharAppSample/GlobalUsing.cs
+++ src/NeuCharAppSample/NeuCharAppSample/GlobalUsing.cs
@@ -2,3 +2,6 @@
 global using Senparc.Weixin.AspNet;
 global using Senparc.Weixin.MP;
 global using Senparc.Weixin.MP.MessageHandlers.Middleware;
+global using Microsoft.Extensions.Configuration;
+global using NeuCharAppSample.Configs;
+global using NeuCharAppSample.MessageHandlers;
--- src/NeuCharAppSample/NeuCharAppSample/Program.cs
+++ src/NeuCharAppSample/NeuCharAppSample/Program.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using NeuCharAppSample.Configs;
-using NeuCharAppSample.MessageHandlers;
-
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
